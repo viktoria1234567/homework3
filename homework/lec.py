9a6a0244249edd40750e5d45f7cae67d032b4dcf
Viktoria <viktoriapredko1@gmail.com>
2022-09-12 19:00:58 +0300

    Solve task4

--- homework/lec.py
+++ homework/lec.py
@@ -15,12 +15,20 @@
 
 #3. Задайте список из вещественных чисел. 
 # Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
-my_list = [1.1, 1.2, 3.1, 5, 10.01]
-min = 1
-max = 0
-for i in my_list:
-    if (i - int(i)) <= min:
-        min = i - int(i)
-    if (i - int(i)) >= max:
-        max = i - int(i)  
-print(max-min)
+# my_list = [1.1, 1.2, 3.1, 5, 10.01]
+# min = 1
+# max = 0
+# for i in my_list:
+#     if (i - int(i)) <= min:
+#         min = i - int(i)
+#     if (i - int(i)) >= max:
+#         max = i - int(i)  
+# print(max-min)
+
+#4.Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+# a = int(input("Введите число: "))
+# res = ""
+# while a != 0:
+#     res = str(a % 2) + res
+#     a = a//2
+# print (res)
