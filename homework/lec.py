ac7987d92067056406ac9b52aa222e4bd917b172
Viktoria <viktoriapredko1@gmail.com>
2022-09-13 19:26:35 +0300

    Solve task5

--- homework/lec.py
+++ homework/lec.py
@@ -32,3 +32,21 @@
 #     res = str(a % 2) + res
 #     a = a//2
 # print (res)
+
+#5.Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+# n = int(input('Введите число: '))
+
+# def fibonacci(n):
+#     num = []
+#     a, b = 1, 1
+#     for i in range(n-1):
+#         num.append(a)
+#         a, b = b, a + b
+#     a, b = 0, 1
+#     for i in range (n):
+#         num.insert(0, a)
+#         a, b = b, a - b
+#     return num
+
+# num = fibonacci(n)
+# print(fibonacci(n))
